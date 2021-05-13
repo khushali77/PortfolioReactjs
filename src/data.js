c6ea53f5f36e1c4078864642c2cf1f5709efb779
khushali77 <201801264@daiict.ac.in>
2021-05-13 20:15:58 +0530

    Added: Blogs Page

--- src/data.js
+++ src/data.js
@@ -15,4 +15,31 @@ export const projectlist = [
     },
 ]
 
-export default projectlist;
+export const bloglist = [
+    {
+        "id":"1",
+        "name": "Week 1 : Documenting the journey of Learning.",
+        "link":"https://khushaliiishahh.medium.com/week-1-documenting-the-journey-of-learning-fd03bc9b90de"
+    },
+    {
+        "id":"2",
+        "name": "Week 2 : Way towards making Frontend as my back !",
+        "link":"https://khushaliiishahh.medium.com/week-2-way-towards-making-frontend-as-my-back-4b89b61b53e9"
+    },
+    {
+        "id":"3",
+        "name": "Week 3 : Taking a look — React.js",
+        "link":"https://khushaliiishahh.medium.com/week-3-taking-a-look-react-js-33a653c4d2f3"
+    },
+    {
+        "id":"4",
+        "name": "Week 4: Hailing to React.js",
+        "link":"https://khushaliiishahh.medium.com/week-4-hailing-to-react-js-cbd004f1553"
+    },
+    {
+        "id":"5",
+        "name": "How To Deal with Yourself when You are not Ready",
+        "link":"https://khushaliiishahh.medium.com/how-to-deal-with-yourself-when-you-are-not-ready-aa262faf953f"
+    }
+]
+
