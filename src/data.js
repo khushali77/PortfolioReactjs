6c8a26db189c76823f36213909af9c8fe9e67285
khushali77 <201801264@daiict.ac.in>
2021-05-13 19:39:20 +0530

    Added:Projects page

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const projectlist = [
+    {
+        "id":"1",
+        "name": "Ecommerce Cloth Retailer",
+        "link": "https://group19-vastra-com.vercel.app/",
+        "github": "https://github.com/khushali77/Group19_Vastra.com",
+        "tech": ["MERN Stack"],
+    },
+    {
+        "id":"2",
+        "name":"Beat Your Speed Game",
+        "link":"https://khushali77.github.io/BeatYourSpeed/",
+        "github":"https://github.com/khushali77/BeatYourSpeed",
+        "tech":["HTML","CSS","JavaScript"],
+    },
+]
+
+export default projectlist;
